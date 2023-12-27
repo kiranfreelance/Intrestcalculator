f8d58a0f2f56f92cd2233153228a7a1a95079462
kiranRHOR <kiran.ankani@torinit.ca>
2023-12-27 13:07:25 +0530

    raju data added

--- src/helper.js
+++ src/helper.js
@@ -41,6 +41,13 @@ export const actualList = [
   // { name: "Bike", date: "04/12/2021", amount: 50000, rate: "2.00" ,status:'paid'},
   // { name: "Bike", date: "01/08/2022", amount: 100000, rate: "2.50" ,status:'paid'},
   {
+    name: "Raju",
+    date: "23/12/2023",
+    amount: 100000,
+    rate: "2.00",
+    status: true, 
+  },
+  {
     name: "Durga Rao",
     date: "09/12/2023",
     amount: 100000,
